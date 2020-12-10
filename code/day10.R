ad91467a071d0b0fd6ad577609276c9f9c808031
Katerina <katerina.boufea@outlook.com>
2020-12-10 18:14:02 +0000

    Replace for loop

--- code/day10.R
+++ code/day10.R
@@ -1,10 +1,10 @@
 # input <- sort(c(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4))
 
-input <- sort(c(
-    28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49,
-    45, 19, 38, 39, 11, 1, 32, 25, 35,8, 17, 7, 9, 4,
-    2, 34, 10, 3
-))
+# input <- sort(c(
+#     28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49,
+#     45, 19, 38, 39, 11, 1, 32, 25, 35,8, 17, 7, 9, 4,
+#     2, 34, 10, 3
+# ))
 
 input <- sort(read.delim(
     "aoc2020/data/day10_input.txt", 
@@ -27,8 +27,8 @@ s <- strsplit(paste(res, collapse = ""), "13")[[1]]
 s <- unlist(lapply(s, function(x) strsplit(x, "3")))
 s <- stringi::stri_remove_empty(s)
 
-n <- c()
-for (v in s) {
+
+count_combinations <- function(v) {
     combinations <- apply(
         expand.grid(replicate(nchar(v), c(0,1), simplify=FALSE)), 
         1, 
@@ -45,6 +45,6 @@ for (v in s) {
         }
         combinations <- combinations[-unique(remove)]
     }
-    n <- c(n, length(combinations))
+    length(combinations)
 }
 prod(n)
